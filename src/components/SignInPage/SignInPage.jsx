9c6cc088417341e74f36956c103cf2888689a8f2
dmitry ermolenko <dmitrijermolenko93@gmail.com>
2020-09-27 21:42:41 +0300

    реализовал клиентскую валидацию для формы логина

--- src/components/SignInPage/SignInPage.jsx
+++ src/components/SignInPage/SignInPage.jsx
@@ -1,26 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
 import { Input, Button } from "antd";
 import classes from "./SignInPage.module.scss";
 
 const SignInPage = () => {
+  const { control, handleSubmit, errors } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = (data) => {};
+
   return (
     <div className={classes.signin}>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
         <h1 className={classes["signin__title"]}>Sign In</h1>
         <label htmlFor="email">Email Address</label>
-        <Input
-          className={classes["signin__input"]}
-          id="email"
-          placeholder="Email address"
-          type="email"
+        {errors.email && (
+          <p style={{ margin: 0, color: "red" }}>{errors.email.message}</p>
+        )}
+        <Controller
+          name="email"
+          control={control}
+          as={
+            <Input
+              className={classes["signin__input"]}
+              id="email"
+              placeholder="Email address"
+              type="email"
+            />
+          }
+          rules={{
+            required: "Email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+              message: "Invalid email format",
+            },
+          }}
         />
+
         <label htmlFor="password">Password</label>
-        <Input
-          className={classes["signin__input"]}
-          id="password"
-          placeholder="Password"
-          type="password"
+        {errors.password && (
+          <p style={{ margin: 0, color: "red" }}>{errors.password.message}</p>
+        )}
+        <Controller
+          name="password"
+          control={control}
+          as={
+            <Input
+              className={classes["signin__input"]}
+              id="password"
+              placeholder="Password"
+              type="password"
+            />
+          }
+          rules={{
+            required: "Password is required",
+          }}
         />
         <Button
           className={classes["signin__submit"]}
