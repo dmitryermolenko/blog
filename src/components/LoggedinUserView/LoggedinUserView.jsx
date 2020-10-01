3e04a2b7207994b0e721239b0350eb7cafe2db34
dmitry ermolenko <dmitrijermolenko93@gmail.com>
2020-10-01 11:42:43 +0300

    реализовал разлогирование

--- src/components/LoggedinUserView/LoggedinUserView.jsx
+++ src/components/LoggedinUserView/LoggedinUserView.jsx
@@ -1,18 +1,26 @@
 import React from "react";
-import UserInfo from "../UserInfo/UserInfo";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import UserInfo from "../UserInfo/UserInfo";
+import setUser from "../../actions/actions";
 import classes from "../Header/Header.module.scss";
 
-const LoggedinUserView = () => {
+const LoggedinUserView = ({ setUser }) => {
   return (
     <div className={classes["header__loggedin"]}>
       <Link className={classes["header__create-article"]} to="/new-article">
         Create article
       </Link>
       <UserInfo />
-      <button className={classes["header__logout"]}>Log Out</button>
+      <button className={classes["header__logout"]} onClick={() => setUser({})}>
+        Log Out
+      </button>
     </div>
   );
 };
 
-export default LoggedinUserView;
+const mapDispatchToProps = {
+  setUser,
+};
+
+export default connect(null, mapDispatchToProps)(LoggedinUserView);
